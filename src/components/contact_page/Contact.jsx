3cbbf092267c54de7799d59e657b3096fcebd253
Acloyer <93879175+Acloyer@users.noreply.github.com>
2024-05-09 03:28:41 +0400

    Fixes in contact to connect with icons

--- src/components/contact_page/Contact.jsx
+++ src/components/contact_page/Contact.jsx
@@ -2,27 +2,25 @@ import React from "react";
 import Navbar from "../navbar/Navbar";
 import Footer from "../footer/Footer";
 
-function Contact() {
-    return (
+function Contact(){
+    return(
         <>
-            <Navbar />
-
-            <div className="qr-code-container">
-                <div className="qr-code" style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", padding: "20px", textAlign: "center" }}>
-                    <img src="instagram.png" alt="Instagram" style={{ width: "400px", height: "400px" }} />
-                    <h1 style={{ textShadow: "1px 1px 1px rgba(0,0,0,0.5)" }}>Instagram</h1>
-                </div>
-                <div className="qr-code" style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", padding: "20px", textAlign: "center" }}>
-                    <img src="github.png" alt="GitHub" style={{ width: "400px", height: "400px" }} />
-                    <h1 style={{ textShadow: "1px 1px 1px rgba(0,0,0,0.5)" }}>GitHub</h1>
-                </div>
-                <div className="qr-code" style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", padding: "20px", textAlign: "center" }}>
-                    <img src="step.png" alt="Step" style={{ width: "400px", height: "400px" }} />
-                    <h1 style={{ textShadow: "1px 1px 1px rgba(0,0,0,0.5)" }}>Step</h1>
-                </div>
+        <Navbar/>
+        <div className="qr-code-container animate__animated animate__zoomInDown">
+            <div className="qr-code">
+                <img src="instagram.png" alt="Instagram" />
+                <h1 style={{ textShadow: "1px 1px 1px rgba(0,0,0,0.5)" }}>Instagram</h1>
             </div>
-
-            <Footer />
+            <div className="qr-code">
+                <img src="github.png" alt="GitHub" />
+                <h1 style={{ textShadow: "1px 1px 1px rgba(0,0,0,0.5)" }}>GitHub</h1>
+            </div>
+            <div className="qr-code">
+                <img src="step.png" alt="Step"/>
+                <h1 style={{ textShadow: "1px 1px 1px rgba(0,0,0,0.5)" }}>Step it</h1>
+            </div>
+        </div>
+        <Footer/>
         </>
     )
 }
