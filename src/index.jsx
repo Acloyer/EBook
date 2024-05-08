027c7e4503b337cb2f9b9feeea3fa719ca32602a
Acloyer <93879175+Acloyer@users.noreply.github.com>
2024-05-09 03:10:47 +0400

    Updated ScrictMode to PrimeProvider

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// prime react
+import { PrimeReactProvider } from 'primereact/api';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';  
+import 'primereact/resources/primereact.css';
+import 'primereact/resources/themes/saga-orange/theme.css';
+//
+
+import './flags.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
 );
 
 reportWebVitals();
