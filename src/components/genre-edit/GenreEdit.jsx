d11b5469b1f30b947e62db6a1827d9b0b7d5d087
Acloyer <93879175+Acloyer@users.noreply.github.com>
2024-05-10 02:02:08 +0400

    Create GenreEdit.jsx

---
+++ src/components/genre-edit/GenreEdit.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Footer from "../footer/Footer";
+import Navbar from "../navbar/Navbar";
+
+function GenreEdit() {
+    return(
+        <>
+            <Navbar/>
+            <div className="container animate__animated animate__zoomInUp">
+                <h2>Genre Edit</h2>
+                <form>
+                    
+                </form>
+            </div>
+            <Footer/>
+        </>
+    )
+}
+
+export default GenreEdit;
