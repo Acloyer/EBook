6711d8f2f3552f311fdbbd898b5949ed7d781e51
Acloyer <93879175+Acloyer@users.noreply.github.com>
2024-05-18 23:44:32 +0400

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,6 +16,99 @@ function Navbar() {
                 console.error("Error fetching genres:", error);
             });
     }, []);
+<<<<<<< Updated upstream
+=======
+
+    const fetchGenres = async () => {
+        try {
+            const response = await axios.get(`${config.backApi}/genres`);
+            setGenres(response.data);
+        } catch (error) {
+            console.error("Error fetching genres:", error);
+        }
+    };
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [userInfo, setUserInfo] = useState(null);
+
+    // carts length
+    const [cartsLength, setCartsLength] = useState(0);
+    //
+    useEffect(() => {
+        const checkAuthentication = async () => {
+            const accessToken = getCookie("accessToken");
+            const refreshToken = getCookie("refreshToken");
+
+            if (accessToken && refreshToken) {
+                try {
+                    const decodedToken = decodeToken(accessToken);
+
+                    if (decodedToken.exp * 1000 > Date.now()) {
+                        setIsLoggedIn(true);
+                        setUserInfo({
+                            firstName: decodedToken.given_name,
+                            lastName: decodedToken.family_name,
+                            email: decodedToken.email,
+                            status: decodedToken["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]
+                        });
+                        // status out
+                        //
+                    } else {
+                        logout();
+                    }
+                } catch (error) {
+                    console.error("Error decoding token:", error);
+                }
+            }
+        };
+
+        checkAuthentication();
+
+        const getCarts = async () => {
+            const accessToken = getCookie("accessToken");
+            const cartResponse = await axios.get(`${config.backApi}/carts`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+            try{
+                setCartsLength(cartResponse.data.items.length);
+            } catch(error) {
+                console.error("Error puting carts length: ", error)
+            }
+        }
+    }, []);
+
+    const getCookie = (name) => {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(";").shift();
+    };
+
+    const decodeToken = (token) => {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        return JSON.parse(atob(base64));
+    };
+
+    const logout = () => {
+        document.cookie = "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        document.cookie = "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        setIsLoggedIn(false);
+        setUserInfo(null);
+    };
+
+    //
+    const [searchValue, setSearchValue] = useState('');
+
+    const handleSearch = () => {
+        if (searchValue.trim() !== '') {
+            const encodedSearchValue = encodeURIComponent(searchValue);
+            window.location.href = `/shop?search=${encodedSearchValue}`;
+        }
+    };
+    //
+>>>>>>> Stashed changes
     return (
         <>
             <div className="animate__animated animate__zoomInDown">
@@ -69,7 +162,13 @@ function Navbar() {
                                     <a href="#" className="navbar-link">Genres</a>
                                     <ul className="dropdown-menu animate__animated animate__bounceIn">
                                         {genres.map(genre => (
+<<<<<<< Updated upstream
                                             <a href="#" className="navbar-link">{genre.name}</a>
+=======
+                                            <li key={genre.id}>
+                                                <Link to={`${config.frontendIP}/Shop?genreId=${genre.id}`} className="navbar-link">{genre.name}</Link>
+                                            </li>
+>>>>>>> Stashed changes
                                         ))}
                                     </ul>
                                 </li>
@@ -77,11 +176,97 @@ function Navbar() {
                                 <li>
                                     <a href="#" className="navbar-link">About Us</a>
                                 </li>
+<<<<<<< Updated upstream
 
                                 <li>
                                     <a href="#" className="navbar-link">Contact</a>
                                 </li>
 
+=======
+                                {isLoggedIn ? (
+                                    <div className="ml-auto" style={{padding: "0px 300px"}}>
+                                        <div className="user-info">
+                                            <div className="user-icons">
+                                                <div className="basket">
+                                                    <div className="item-counter">
+                                                        <p>{cartsLength}</p>
+                                                    </div>
+                                                    <Link className="navbar-link" to='/basket'>
+                                                        <BasketOutline
+                                                            color={'#00000'}
+                                                            className="basket-icon"
+                                                            height="auto"
+                                                            width="30px" />
+                                                    </Link>
+                                                </div>
+                                                <div className="favorites">
+                                                    <Link className="navbar-link" to='/favorite'>
+                                                        <HeartOutline
+                                                            color={'#00000'}
+                                                            height="auto"
+                                                            width="25px"
+                                                        />
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                            <div className="user-photo">
+                                                <img src="user.png" />
+                                            </div>
+                                            <div className="user-detail">
+                                                <h4>{userInfo.firstName} {userInfo.lastName}</h4>
+                                                <h5>{userInfo.email}</h5>
+                                            </div>
+                                            <div
+                                                className="dropdown-shape"
+                                                onMouseEnter={handleMouseEnter}
+                                                onMouseLeave={handleMouseLeave}
+                                                >
+                                                <ChevronDownOutline 
+                                                    className="shape"
+                                                    height="auto"
+                                                    width="25px"/>
+                                                {dropdownOpen && (
+                                                    <div className="shape-dropdown">
+                                                        <ul className="animate__animated animate__bounceIn">
+                                                            <li>
+                                                                <a className="navbar-link shape-link" href="/Profile">
+                                                                    <OptionsOutline 
+                                                                        color={'#00000'}/>
+                                                                </a>
+                                                            </li>
+                                                            <li>
+                                                                <a className="navbar-link shape-link" href="#" onClick={logout}>
+                                                                    <ExitOutline 
+                                                                        color={'#00000'}/>
+                                                                </a>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                )}
+                                            </div>
+                                        </div>
+                                    </div>
+                                ) : (
+                                    <li>
+                                        <div className="nav-icons" style={{padding: "0px 400px"}}>
+                                            <div className="favorites">
+                                                <Link className="navbar-link" to='/'>
+                                                    <HeartOutline
+                                                        color={'#00000'}
+                                                        height="auto"
+                                                        width="25px"
+                                                    />
+                                                </Link>
+                                            </div>
+                                            <span className="material-symbols-outlined">
+                                                <Link className="navbar-auth" to='/Authentication'>
+                                                    <a>person_add</a>
+                                                </Link>
+                                            </span>
+                                        </div>
+                                    </li>
+                                )}
+>>>>>>> Stashed changes
                             </ul>
 
                             <ul className="navbar-list">
